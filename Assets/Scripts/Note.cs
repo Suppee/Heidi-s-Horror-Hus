3c1a6ce23eb363a44fadb5571c860761ddd5d7dd
Marinus Bach Oestrich <maroes21@student.sdu.dk>
2022-04-22 14:01:37 +0200

    Note System

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Note : Interactable
 {
+    public GameObject noteImage;
+
     [SerializeField] private AudioClip ItemCollected;
     [SerializeField] private AudioSource audioSource;
 
+    public GameObject noteLibrary;
+    private SpriteRenderer noteReference;
+    public string ID;
+
+    private void Start()
+    {
+        noteImage.SetActive(false);
+        noteReference = noteLibrary.transform.Find(ID).gameObject.GetComponent<SpriteRenderer>();
+        if (noteReference != null)
+            noteImage.GetComponent<Image>().sprite = noteReference.sprite;
+    }
+
     public override void Interact()
     {
-        if (GetComponent<AudioSource>().isPlaying == false)
+        if (noteImage.activeSelf == false)
         {
-            audioSource.clip = ItemCollected;
-            audioSource.Play();
+            noteImage.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(ItemCollected);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            playerController.canControl = false;
         }
-        gameObject.SetActive(false);
+        else
+        {
+            noteImage.SetActive(false);
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            playerController.canControl = true;
+        }
     }
 }
